ca06cd6ffac3a41af3ca398482b4824434588fbd
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-01-05 10:56:51 -0600

    Checkpoint completion of DHT11 Sensor Acquisition subroutine implementation.

--- main.cpp
+++ main.cpp
@@ -25,6 +25,9 @@
 #define LED_ON  1
 #define LED_OFF 0
 
+static const uint32_t DHT11_DEVICE_USER_OBSERVABILITY_DELAY(3); // 3 seconds.
+static const float    DHT11_DEVICE_STATE_CHANGE_RATE(1.0);      // 1 second.
+
 // DHT11 uses a simplified single-wire bidirectional communication protocol.
 // It follows a Master/Slave paradigm with the MCU observing these states:
 // WAITING, READING.
@@ -34,41 +37,98 @@ static const uint32_t DHT11_DEVICE_READING(1UL);
 // TBD Nuertey Odzeyem; ensure to protect all prints with the Utility::g_STDIOMutex
 // TBD Nuertey Odzeyem; move all the DHT11 processing into its own function or lambda
 // TBD Nuertey Odzeyem; consider moving the LCD 16x2 outputting too into its own function or lambda
+// TBD Nuertey Odzeyem; check all pin definitions to make sure that they actually match your physical mock-up.
 
 DHT11             g_DHT11(PB_8);     // DHT11 --> PB_8
-                  
+          
+// As per my ARM NUCLEO-F767ZI specs:        
 DigitalOut        g_LEDGreen(LED1);
 DigitalOut        g_LEDBlue(LED2);
 DigitalOut        g_LEDRed(LED3);
 
-// Use the LowPowerTicker interface to set up a recurring interrupt. 
-// It calls a function repeatedly and at a specified rate.
-LowPowerTicker    g_Ticker;
+// Use the Ticker interface to set up a recurring interrupt. It calls a
+// function repeatedly and at a specified rate. As we don't need 
+// microseconds precision anyway, leverage LowPowerTicker instead. 
+// Crucially, that would ensure that we do not block deep sleep mode.
+LowPowerTicker    g_PeriodicStateChanger;
 
 volatile uint32_t g_DeviceState;     // Indicates when to read in a new sample.
 
-void ChangeDeviceState();
+// ==========================================================
+// Free-Floating General Helper Functions To Be Used By All :
+// ==========================================================
+using DHT11StatusCodesMap_t = std::map<DHT11::DHT11_status_t, std::string>;
+using IndexElementDHT11_t   = DHT11StatusCodesMap_t::value_type;
+
+inline static auto make_dht11_error_codes_map()
+{
+    DHT11StatusCodesMap_t eMap;
+    
+    eMap.insert(IndexElementDHT11_t(DHT11_SUCCESS, std::string("\"Communication success\"")));
+    eMap.insert(IndexElementDHT11_t(DHT11_FAILURE, std::string("\"Communication failure\"")));
+    eMap.insert(IndexElementDHT11_t(DHT11_DATA_CORRUPTED, std::string("\"Checksum error\"")));
+    eMap.insert(IndexElementDHT11_t(DHT11_BUS_TIMEOUT, std::string("\"Bus response timeout/error\"")));
+ 
+    return eMap;
+}
+
+static DHT11StatusCodesMap_t gs_DHT11StatusCodesMap = make_dht11_error_codes_map();
+
+inline std::string ToString(const DHT11::DHT11_status_t & key)
+{
+    return (gs_DHT11StatusCodesMap.at(key));
+}
+
+// ==============================
+// Begin Actual Implementations :
+// ==============================
+void AlertToRead()
+{
+    g_DeviceState = DHT11_DEVICE_READING;
+}
 
 void DHT11SensorAcquisition()
 {
-    DHT11::DHT11_status_t aux;
-    DHT11::DHT11_data_t g_DHT11_Data;
+    DHT11::DHT11_status_t result;
+    DHT11::DHT11_data_t   theDHT11Data;
 
     // Indicate with the green LED that DHT11 processing is about to begin.
     g_LEDGreen = LED_ON;
-    ThisThread::sleep_for(3);
+    ThisThread::sleep_for(DHT11_DEVICE_USER_OBSERVABILITY_DELAY);
     g_LEDGreen = LED_OFF;
 
     // DHT11 processing begins. Therefore first release the single-wire 
     // bidirectional bus:
-    aux = g_DHT11.DHT11_Init();
+    result = g_DHT11.DHT11_Init();
+    if (result != DHT11_SUCCESS)
+    {
+        Utility::g_STDIOMutex.lock();
+        printf("Error! g_DHT11.DHT11_Init() returned: [%d] -> %s\n", 
+              result, ToString(result).c_str());
+        Utility::g_STDIOMutex.unlock();
+
+        return;
+    }
 
     g_DeviceState = DHT11_DEVICE_WAITING;
-    g_Ticker.attach(&ChangeDeviceState, 1U); // the address of the function to be attached ( changeDATA ) and the interval ( 1s )
 
-    // Let the callbacks take care of everything
-    while(1)
+    // Interrupt us every second to wake us up:
+    g_PeriodicStateChanger.attach(&AlertToRead, DHT11_DEVICE_STATE_CHANGE_RATE);
+
+    while (1)
     {
+        // Power Management (sleep)
+        //
+        // Sleep mode
+        //
+        // The system clock to the core stops until a reset or an interrupt 
+        // occurs. This eliminates dynamic power that the processor, 
+        // memory systems and buses use. This mode maintains the processor,
+        // peripheral and memory state, and the peripherals continue to work
+        // and can generate interrupts.
+        //
+        // You can wake up the processor by any internal peripheral interrupt
+        // or external pin interrupt.
         sleep();
 
         if (g_DeviceState == DHT11_DEVICE_READING)
@@ -76,24 +136,27 @@ void DHT11SensorAcquisition()
             // Indicate that we are reading from DHT11 with blue LED.
             g_LEDBlue = LED_ON;
 
-            // Get a new data
-            aux = g_DHT11.DHT11_GetData(&g_DHT11_Data);
+            result = g_DHT11.DHT11_GetData(&theDHT11Data);
 
-            // Depend on checksum in trusting data read from DHT11 device.
-            std::string((g_DHT11_Data.checksumStatus == DHT11::DHT11_CHECKSUM_OK) ? "PASSED" : "FAILED") checksum;
+            if (result != DHT11_SUCCESS)
+            {
+                Utility::g_STDIOMutex.lock();
+                printf("Error! g_DHT11.DHT11_GetData() returned: [%d] -> %s\n", 
+                      result, ToString(result).c_str());
+                Utility::g_STDIOMutex.unlock();
+            }
+            else
+            {
+                // Depend on checksum in trusting data read from DHT11 device.
+                std::string((theDHT11Data.checksumStatus == DHT11::DHT11_CHECKSUM_OK) ? "PASSED" : "FAILED") checksum;
 
-            Utility::g_STDIOMutex.lock();
-            printf("Temperature: %d\tHumidity : %d%% RH\tChecksum: %s\r\n", g_DHT11_Data.temperature, g_DHT11_Data.humidity, checksum.c_str());
-            Utility::g_STDIOMutex.unlock();
+                Utility::g_STDIOMutex.lock();
+                printf("Temperature: %d\tHumidity : %d%% RH\tChecksum: %s\r\n", theDHT11Data.temperature, theDHT11Data.humidity, checksum.c_str());
+                Utility::g_STDIOMutex.unlock();
+            }
 
             g_LEDBlue = LED_OFF;
-
             g_DeviceState = DHT11_DEVICE_WAITING;
         }
     }
-}
-
-void ChangeDeviceState()
-{
-    g_DeviceState = DHT11_DEVICE_READING;
 }
